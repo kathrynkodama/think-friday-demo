fc57c6ed117564a5f9036fb675267b5207e0c351
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2021-05-03 11:01:04 -0400

    Add MP Health 3.0 checks
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

---
+++ src/main/java/io/openliberty/sample/javax/finish/InventoryLivenessCheck.java
@@ -0,0 +1,24 @@
+package io.openliberty.sample.javax.finish;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class InventoryLivenessCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        MemoryMXBean memBean = ManagementFactory.getMemoryMXBean();
+        long memUsed = memBean.getHeapMemoryUsage().getUsed();
+        long memMax = memBean.getHeapMemoryUsage().getMax();
+
+        return HealthCheckResponse.named(InventoryResource.class.getSimpleName() + " Liveness Check")
+                .withData("memory used", memUsed).withData("memory max", memMax).status(memUsed < memMax * 0.9).build();
+    }
+}
---
+++ src/main/java/io/openliberty/sample/javax/finish/InventoryReadinessCheck.java
@@ -0,0 +1,20 @@
+package io.openliberty.sample.javax.finish;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Readiness
+@ApplicationScoped
+public class InventoryReadinessCheck implements HealthCheck {
+
+    private static final String readinessCheck = InventoryResource.class.getSimpleName() + " Readiness Check";
+    
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named(InventoryReadinessCheck.class.getSimpleName()).withData("ready", true).up()
+                .build();
+    }
+}
